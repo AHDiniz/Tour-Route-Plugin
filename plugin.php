66251dc950906f760896502dc7090a8801c974e5
Alan Diniz <alanhdiniz@gmail.com>
2022-02-18 17:07:50 -0300

    Adding files

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Tour Route
+ * Plugin URI: https://github.com/AHDiniz/Tour-Route-Plugin
+ * Description: Adds tour activities and tour routes to your site
+ * Version: 0.0.0
+ * Author: Alan Herculano Diniz
+ * Author URI: http://www.alanhdiniz.com
+ * License: MIT
+ */
+
+?>
